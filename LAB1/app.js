d8f39639f94a7a66e5da02d547d0a512ad6ccfdc
Chollathit Saensaradee <huntgame2v2@gmail.com>
2022-07-06 20:31:51 +0700

    finish

--- LAB1/app.js
+++ LAB1/app.js
@@ -27,7 +27,7 @@ app.get('/api/users', (req, res) => {
     return res.json(result)
 })
 
-app.get("/api/users/:id", (req, res) => {
+app.get("/api/user/:id", (req, res) => {
     let user = users.find(user => user.id === parseInt(req.params.id));
     if (!user)
         return res.status(400).json({ status: 400, massage: "Not found user with the given ID" });
@@ -35,6 +35,53 @@ app.get("/api/users/:id", (req, res) => {
     const result = {
         status: 200,
         data: res.user
+    };
+    return res.json(result);
+})
+
+app.post('/api/users',(req, res) => {
+    let user = {
+        "id":users.length+1,
+        "name":req.body.name,
+        "email":req.body.email
+    }
+    users.push(user)
+    const result = {
+        "status":200,
+        "data":users
+    }
+    return res.json(result)
+})
+
+app.put("/api/user/:id", (req, res) => {
+    let user = users.find(user => user.id === parseInt(req.params.id));
+    if (!user)
+        return res.status(400).json({ status: 400, message: "Not found user with whe given ID" });
+
+    let user_index = users.findIndex(user => user.id === parseInt(req.params.id))
+    user = {
+        "id": user.id,
+        "name": req.body.name,
+        "email": req.body.email
+    }
+    users[user_index] = user
+    const result = {
+        "status": 200,
+        "data": user
+    }
+    return res.json(result)
+})
+
+app.delete("/api/user/:id", (req, res) => {
+    let user = users.find(user => user.id === parseInt(req.params.id));
+    if (!user)
+        return res.status(400).json({ status: 400, message: "Not found user with whe given ID" });
+
+    let user_index = users.findIndex(user => user.id === parseInt(req.params.id));
+    users.pop(user_index);
+    const result = {
+        "status": 200,
+        "data": users
     };
     return res.json(result);
 })
